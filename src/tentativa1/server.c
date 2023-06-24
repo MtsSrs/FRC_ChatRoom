6fcb52a6d99a098e2fd3a142cc7c5ba7da8235e3
phrezende-eng <ph.hr.001@gmail.com>
2023-06-24 17:52:05 -0300

    - init

--- src/tentativa1/server.c
+++ src/tentativa1/server.c
@@ -1,172 +1,173 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
 
-#define BUFFER_SIZE 2048
+#define MAX_NAME_LENGTH 50
 #define MAX_CLIENTS 10
-#define MAX_NAME_LENGTH 32
+#define BUFFER_SIZE 1024
 
 typedef struct
 {
     char name[MAX_NAME_LENGTH];
     int room;
-    int sockfd; // Novo campo para armazenar o descritor de arquivo do socket
+    int sockfd;
 } ClientData;
 
-void parseProtocolBuffer(char *protocol_buffer, char *userName, int *room)
-{
-    sscanf(protocol_buffer, "%[^:]: %x", userName, room);
-}
-
-void sendMessageToRoomClients(ClientData *clients, int numClients, int room, char *message)
+void sendMessageToRoomClients(ClientData *client, ClientData *clients, int numClients, const char *message)
 {
     for (int i = 0; i < numClients; i++)
     {
-        if (clients[i].room == room)
+        if (clients[i].room == client->room && clients[i].sockfd != client->sockfd)
         {
             send(clients[i].sockfd, message, strlen(message), 0);
         }
     }
 }
 
-int main(int argc, char **argv)
+void *threadFunction1(void *arg)
 {
-    int sockfd = 0;
-    int clientSockfd = 0;
-    char protocol_buffer[BUFFER_SIZE];
+    int sockfd = *(int *)arg;
+    char buffer[BUFFER_SIZE];
     ClientData clients[MAX_CLIENTS];
     int numClients = 0;
 
-    char *ip = argv[1];
-    char *port = argv[2];
-
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t clientAddrLen;
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    while (1)
     {
-        printf("Erro ao criar o socket\n");
+        // Receber nome do cliente
+        int bytesRead = recv(sockfd, buffer, BUFFER_SIZE - 1, 0);
+        if (bytesRead <= 0)
+        {
+            // Erro ou conexão fechada pelo cliente
+            break;
+        }
+        buffer[bytesRead] = '\0';
+
+        // Adicionar cliente à lista
+        strcpy(clients[numClients].name, buffer);
+        clients[numClients].room = 1; // Definir sala fixa para exemplo
+        clients[numClients].sockfd = sockfd;
+        numClients++;
+
+        // Enviar mensagem de entrada para os outros clientes
+        char message[BUFFER_SIZE];
+        snprintf(message, BUFFER_SIZE, "%s entrou na sala.\n", buffer);
+        sendMessageToRoomClients(&clients[numClients - 1], clients, numClients, message);
+    }
+
+    // Cliente desconectado, remover da lista
+    for (int i = 0; i < numClients; i++)
+    {
+        if (clients[i].sockfd == sockfd)
+        {
+            // Enviar mensagem de saída para os outros clientes
+            char message[BUFFER_SIZE];
+            snprintf(message, BUFFER_SIZE, "%s saiu da sala.\n", clients[i].name);
+            sendMessageToRoomClients(&clients[i], clients, numClients, message);
+
+            // Remover cliente da lista
+            for (int j = i; j < numClients - 1; j++)
+            {
+                clients[j] = clients[j + 1];
+            }
+            numClients--;
+            break;
+        }
+    }
+
+    close(sockfd);
+    return NULL;
+}
+
+void *threadFunction2(void *arg)
+{
+    ClientData *client = (ClientData *)arg;
+    char buffer[BUFFER_SIZE];
+    ClientData *clients = (ClientData *)arg;                       // Adicionar declaração de clients
+    int numClients = *(int *)((char *)arg + sizeof(ClientData *)); // Adicionar declaração de numClients
+
+    while (1)
+    {
+        int bytesRead = recv(client->sockfd, buffer, BUFFER_SIZE - 1, 0);
+        if (bytesRead <= 0)
+        {
+            // Erro ou conexão fechada pelo cliente
+            break;
+        }
+        buffer[bytesRead] = '\0';
+
+        // Enviar mensagem para os outros clientes na sala
+        char message[BUFFER_SIZE];
+        snprintf(message, BUFFER_SIZE, "%s: %s", client->name, buffer);
+        sendMessageToRoomClients(client, clients, numClients, message);
+    }
+
+    close(client->sockfd);
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Uso: %s [IP] [PORTA]\n", argv[0]);
         return EXIT_FAILURE;
     }
 
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Erro ao criar socket");
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
     serverAddr.sin_addr.s_addr = inet_addr(ip);
-    serverAddr.sin_port = htons(atoi(port));
 
     if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
     {
-        printf("Erro ao fazer o bind\n");
+        perror("Erro ao fazer bind");
         return EXIT_FAILURE;
     }
 
     if (listen(sockfd, MAX_CLIENTS) < 0)
     {
-        printf("Erro ao escutar a porta\n");
+        perror("Erro ao ouvir na porta");
         return EXIT_FAILURE;
     }
 
-    printf("Aguardando conexões...\n");
+    printf("Servidor iniciado. Aguardando conexões...\n");
 
     while (1)
     {
-        clientAddrLen = sizeof(clientAddr);
-        clientSockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen);
-
+        struct sockaddr_in clientAddr;
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        int clientSockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen);
         if (clientSockfd < 0)
         {
-            printf("Erro ao aceitar a conexão\n");
+            perror("Erro ao aceitar conexão");
             return EXIT_FAILURE;
         }
 
-        printf("Conexão estabelecida\n");
-
-        ssize_t bytesRead = recv(clientSockfd, protocol_buffer, BUFFER_SIZE - 1, 0);
-        if (bytesRead < 0)
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, threadFunction1, &clientSockfd) != 0)
         {
-            printf("Erro ao receber dados\n");
+            perror("Erro ao criar thread");
             return EXIT_FAILURE;
         }
 
-        protocol_buffer[bytesRead] = '\0';
-        parseProtocolBuffer(protocol_buffer, clients[numClients].name, &(clients[numClients].room));
-
-        printf("Nome: %s\n", clients[numClients].name);
-        printf("Sala: %d\n", clients[numClients].room);
-
-        clients[numClients].sockfd = clientSockfd; // Armazenar o descritor de arquivo do socket
-
-        numClients++;
-
-        if (numClients >= MAX_CLIENTS)
-        {
-            printf("Número máximo de clientes atingido. Encerrando conexões novas.\n");
-            break;
-        }
-
-        // Enviar mensagem de boas-vindas para o cliente
-        char welcomeMessage[BUFFER_SIZE];
-        sprintf(welcomeMessage, "Bem-vindo, %s! Você está na sala %d\n", clients[numClients - 1].name, clients[numClients - 1].room);
-        send(clientSockfd, welcomeMessage, strlen(welcomeMessage), 0);
-
-        // Enviar mensagem de entrada para outros clientes na mesma sala
-        char enterMessage[BUFFER_SIZE];
-        sprintf(enterMessage, "%s entrou na sala.\n", clients[numClients - 1].name);
-        sendMessageToRoomClients(clients, numClients, clients[numClients - 1].room, enterMessage);
-    }
-
-    printf("Conexões encerradas. Aguardando mensagens...\n");
-
-    while (1)
-    {
-        for (int i = 0; i < numClients; i++)
-        {
-            clientSockfd = clients[i].sockfd;
-
-            ssize_t bytesRead = recv(clientSockfd, protocol_buffer, BUFFER_SIZE - 1, 0);
-            if (bytesRead < 0)
-            {
-                printf("Erro ao receber dados\n");
-                return EXIT_FAILURE;
-            }
-            else if (bytesRead == 0)
-            {
-                // O cliente fechou a conexão
-                printf("%s saiu da sala.\n", clients[i].name);
-
-                // Enviar mensagem de saída para outros clientes na mesma sala
-                char exitMessage[BUFFER_SIZE];
-                sprintf(exitMessage, "%s saiu da sala.\n", clients[i].name);
-                sendMessageToRoomClients(clients, numClients, clients[i].room, exitMessage);
-
-                // Fechar o socket do cliente e remover do array de clientes
-                close(clientSockfd);
-                for (int j = i; j < numClients - 1; j++)
-                {
-                    clients[j] = clients[j + 1];
-                }
-                numClients--;
-                i--;
-            }
-            else
-            {
-                protocol_buffer[bytesRead] = '\0';
-                printf("[%s] Mensagem recebida: %s\n", clients[i].name, protocol_buffer);
-
-                // Enviar mensagem para outros clientes na mesma sala
-                char message[BUFFER_SIZE];
-                sprintf(message, "[%s] %s\n", clients[i].name, protocol_buffer);
-                sendMessageToRoomClients(clients, numClients, clients[i].room, message);
-            }
-        }
+        printf("Novo cliente conectado.\n");
     }
 
     close(sockfd);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
